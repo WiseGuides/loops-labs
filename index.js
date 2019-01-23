c9f47c6c0b669fd89512bac0c76907f0d0ee85e8
Hal Helms <hal.helms@gmail.com>
2019-01-23 11:36:29 -0800

    6 exercises

---
+++ index.js
@@ -0,0 +1,31 @@
+// Loops Lab
+// Rev. 23Jan2019
+
+/*
+1. Use a for loop to count from 100 to 150, outputting the number on each iteration.
+*/
+
+/*
+2. Use a for loop to count backwards from 10 to 1, outputting the number on each iteration.
+*/
+
+/*
+3. Create a while loop that loops until the results of calling randomBoolean returns true. On each iteration, output "Now in interation <n> with a result of <true|false>"
+
+function randomBoolean() {
+  return Math.random() > 0.8;
+}
+*/
+
+/*
+4. The only problem with Exercise 3 is that it might not execute at all. Change to a do...while loop.
+*/
+
+/*
+5. The variable, 'arguments', is available within a function. It's an array-like structure that gives you access to all arguments passed into the function. Create a function that accepts several arguments. Inside the function, use the for...of loop to iterate over arguments, outputting the data type of each argument.
+*/
+
+/*
+6. Use for for...of loop to output the value of each member of this Set:
+let mySet = new Set(1,2,3,5,8,13,21,34,55);
+*/
